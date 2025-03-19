a3f390fd0a9fa8e36f53ab030ee58bc2b1244f0d
R JEEVAN KUMAR <g1kumar215@gmail.com>
2025-03-19 10:53:37 +0530

    My Portfolio Initial Design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className="bg-blue-600 text-white p-4 text-center mt-10">
       <div className="flex justify-center gap-4 mb-2">
+        <a href="https://www.facebook.com/g1kumar.in" target="_blank" rel="noopener noreferrer"><FaFacebook size={24} /></a>
         <a href="https://github.com/G1KUMAR-INDIA" target="_blank" rel="noopener noreferrer"><FaGithub size={24} /></a>
         <a href="https://www.linkedin.com/in/g1kumarr/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={24} /></a>
       </div>
