54f9c4ce81c04d000cdb8704401b5dfe5ca8507a
R JEEVAN KUMAR <g1kumar215@gmail.com>
2025-03-19 10:45:58 +0530

    My Portfolio Initial Design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="bg-white shadow-md p-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold text-blue-600">Reddyvari Jeevan Kumar</h1>
-      <ul className="flex flex-wrap gap-4 text-gray-700">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/skills">Skills</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/education">Education</Link></li>
-        <li><Link to="/experience">Experience</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+    <div className="bg-blue-600 shadow-md p-4 flex justify-between items-center">
+      <h1 className="text-2xl font-bold text-[#D60A6A]">Reddyvari Jeevan Kumar</h1>
+      <ul className="flex flex-wrap gap-4 text-white">
+        <li class="hover:text-[#EF3E42]"><Link to="/">Home</Link></li>
+        <li class="hover:text-[#EF3E42]"><Link to="/about">About</Link></li>
+        <li class="hover:text-[#EF3E42]"><Link to="/skills">Skills</Link></li>
+        <li class="hover:text-[#EF3E42]"><Link to="/projects">Projects</Link></li>
+        <li class="hover:text-[#EF3E42]"><Link to="/education">Education</Link></li>
+        <li class="hover:text-[#EF3E42]"><Link to="/experience">Experience</Link></li>
+        <li class="hover:text-[#EF3E42]"><Link to="/contact">Contact</Link></li>
       </ul>
     </div>
   );
