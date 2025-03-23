a652714660748e00f15a6bffb4ea93520dfde701
R JEEVAN KUMAR <g1kumar215@gmail.com>
2025-03-23 13:19:57 +0530

    Portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="bg-blue-600 shadow-md p-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold text-[#D60A6A]">Reddyvari Jeevan Kumar</h1>
+      <h1 className="text-2xl font-bold text-[#D60A6A]">My Portpolio Website</h1>
       <ul className="flex flex-wrap gap-4 text-white">
         <li class="hover:text-[#EF3E42]"><Link to="/">Home</Link></li>
         <li class="hover:text-[#EF3E42]"><Link to="/about">About</Link></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 const Home = () => {
     return (
-      <section className="p-10 text-center">
-        <h2 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h2>
-        <p className="text-xl">I'm R Jeevan Kumar, Full Stack Developer(MERN+Python).</p>
-      </section>
+      <div className="flex flex-row bg-[#652D19]">
+        <div className="justify-items-center border-2 border-[#D60A6A]" width="560" height="315">
+          <img src="https://shorturl.at/UJJ5L"  alt="G1KUMAR"  className="h-48 border-2 border-[#D60A6A]" />
+          <p className="text-2xl text-red-500">Reddyvari Jeevan Kumar B.Tech(EEE)</p>
+          <p className="text-xl text-yellow-600"  >Full Stack Developer(MERN+Python).</p>        
+        </div>
+        <div className="border-2 border-[#D60A6A]">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/owy9G_8jynQ?si=jUQYM-GQW-puWK0u" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+        <div className="border-2 border-[#D60A6A]">
+        <img src="https://shorturl.at/UJJ5L"  alt="G1KUMAR"  className="h-48 border-2 border-[#D60A6A]" />          
+        </div>
+      </div>
+      
     );
   };
   
